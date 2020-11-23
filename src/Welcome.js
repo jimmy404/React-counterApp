026a0db0f6e70258afc5a781a8814a1803814b65
Jimmy404 <bruno_pier@hotmail.com>
2020-11-23 12:05:18 -0300

    Adds fragment & .gitignore exclusions

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Welcome = () => {
-  return <h1>Welcome Jimmy</h1>
+  return (
+    <>
+      <h1>Welcome Jimmy</h1>
+      <h2>Welcome Jimmy</h2>
+    </>
+  );
 }
 
 export default Welcome;
